aa0c3451fbab0bceb82c1940393726e2b1fce107
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-08-31 19:29:28 -0300

    servicos de login e logout

---
+++ src/main/java/br/com/project/bean/geral/BeanManagedViewAbstract.java
@@ -0,0 +1,12 @@
+package br.com.project.bean.geral;
+
+import org.springframework.stereotype.Component;
+
+import br.com.project.report.util.BeanReportView;
+
+@Component
+public abstract class BeanManagedViewAbstract extends BeanReportView{
+
+	private static final long serialVersionUID = 1L;
+
+}
