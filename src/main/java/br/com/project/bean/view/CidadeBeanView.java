c855de3cb747672098f03d7b7ab89360998864ab
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-09-03 11:48:49 -0300

    Cidade - Repository, Dao, Srv, SrvImpl, Controller e Bean

---
+++ src/main/java/br/com/project/bean/view/CidadeBeanView.java
@@ -0,0 +1,22 @@
+package br.com.project.bean.view;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.project.bean.geral.BeanManagedViewAbstract;
+import br.com.project.geral.controller.CidadeController;
+
+@Controller
+@Scope(value = "session")
+@ManagedBean(name = "cidadeBeanView")
+public class CidadeBeanView extends BeanManagedViewAbstract {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private CidadeController cidadeController;
+
+}
